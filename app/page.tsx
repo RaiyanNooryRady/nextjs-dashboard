d0bd3cebca0622928c61cd10b7c145952053d825
raiyannooryrady <raiyannooryrady@gmail.com>
2025-08-11 11:37:30 +0600

    testing tailwind css

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,12 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
+            <a href="https://nextjs.org/learn/" className="text-blue-500 underline">
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
           </p>
+          <h1 className="text-4xl font-bold text-red-500 ps-10 border-2 border-red-500 rounded-xl">Testing Tailwind</h1>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
